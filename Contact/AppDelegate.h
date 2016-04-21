875aecef872669c08669dd28fe40785b9ae81dc1
Md Adit Hasan <skadithasan19@gmail.com>
2016-04-21 16:15:05 -0500

    Initial Commit

---
+++ Contact/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Contact
+//
+//  Created by Md Adit Hasan on 4/21/16.
+//  Copyright © 2016 Md Adit Hasan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
